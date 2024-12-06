eedf90fbcbf005b1a03781c2b67c333379095d5f
masudulalam <masuudulalam@gmail.com>
2024-12-07 04:05:49 +0600

    added history section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.querySelector('#btn-donate-now').addEventListener('click', function() {
+document.querySelector('#btn-donate-now').addEventListener('click', function () {
     // get donation amount
     let donationAmount = document.querySelector('#input-donation-amount').value;
     // donation amount convert string to number
     donationAmount = parseFloat(donationAmount);
-    if(typeof donationAmount !== 'number' || donationAmount <= 0) {
+    if (typeof donationAmount !== 'number' || donationAmount <= 0) {
         alert('Please provide valid number.');
         return;
     }
@@ -27,18 +27,44 @@ document.querySelector('#btn-donate-now').addEventListener('click', function() {
 });
 
 // display cards event
-document.querySelector('#btn-donation').addEventListener('click', function(){
+document.querySelector('#btn-donation').addEventListener('click', function () {
     // remove the hidden class list from donation cards section 
     document.querySelector('#donation-card').classList.remove('hidden');
     // add the hidden class to the history section
     document.querySelector('#donation-history').classList.add('hidden');
 
 })
-// display history event
-document.querySelector('#btn-history').addEventListener('click', function(){
+
+// show donation history
+document.querySelector('#btn-history').addEventListener('click', function () {
     // remove the hidden class list from history section
     document.querySelector('#donation-history').classList.remove('hidden');
     // add the hidden class to the donation cards section
     document.querySelector('#donation-card').classList.add('hidden');
 
+    // input donation amount
+    let donationAmount = document.querySelector('#input-donation-amount').value;
+    // donation amount convert string to number
+    donationAmount = parseFloat(donationAmount);
+    // donation title
+    const donateTitle = document.querySelector('#donate-title').innerText;
+
+    // Add donation history 
+    // get current date
+    const date = new Date();
+    // create a div
+    const div = document.createElement('div');
+    div.classList.add('border');
+    div.classList.add('rounded-lg');
+    div.classList.add('p-8');
+    div.classList.add('mb-6');
+    div.innerHTML = `
+        <h2 class='text-2xl mb-4'>${donationAmount} Taka is ${donateTitle}</h2>
+        <p>${date}</p>
+    `;
+    // get donation history container
+    const donationHistoryContainer = document.querySelector('#donation-history');
+    donationHistoryContainer.appendChild(div);
+    
 })
+
