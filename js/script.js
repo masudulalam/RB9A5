9a724c8e9cf1c4074ff26a02c7496ea25c9485c9
masudulalam <masuudulalam@gmail.com>
2024-12-06 21:25:49 +0600

    added an event in dontation button that minus the input donation amount fromo initial amountn

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ document.querySelector('#btn-donate-now').addEventListener('click', function() {
     let donationAmount = document.querySelector('#input-donation-amount').value;
     // donation amount convert string to number
     donationAmount = parseFloat(donationAmount);
+    if(typeof donationAmount !== 'number' || donationAmount <= 0) {
+        alert('Please provide valid number.');
+        return;
+    }
     // get previous donation amount
     let preDonationAmount = document.querySelector('#display-donation-amount').innerText;
     // previous donation amount convert string to number
@@ -11,4 +15,13 @@ document.querySelector('#btn-donate-now').addEventListener('click', function() {
     const currentDonationAmount = preDonationAmount + donationAmount;
     // Finally display the current donation amount
     document.querySelector('#display-donation-amount').innerText = currentDonationAmount;
+
+    // get the initial amount Field
+    const initialAmountField = document.querySelector('#initial-amount');
+    // get the initial amount
+    let initialAmount = initialAmountField.innerText;
+    initialAmount = parseFloat(initialAmount);
+    const currentAmount = initialAmount - donationAmount;
+    // set the current amount in initial filed
+    initialAmountField.innerText = currentAmount;
 });
